86e5135ba9c1fc0c5d31b92571a41b60c4920499
balsipson <thirdi@gmail.com>
2023-03-12 20:00:03 +0530

    return body

--- server/api/openai.ts
+++ server/api/openai.ts
@@ -21,30 +21,24 @@ export default defineEventHandler(async (event) => {
     const body = await readBody(event)
     console.log("body", JSON.parse(body))
 
-    const messages = JSON.parse(body).messages
-    let options = JSON.parse(body).options
-    // return event.node.req
+    return body
+    // const messages = JSON.parse(body).messages
+    // let options = JSON.parse(body).options
+    // // return event.node.req
 
-    const prediction = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: messages,
-      max_tokens: options.max_tokens,
-      temperature: options.temperature,
-      frequency_penalty: options.frequency_penalty,
-      presence_penalty: options.presence_penalty
-    });
+    // const prediction = await openai.createChatCompletion({
+    //   model: "gpt-3.5-turbo",
+    //   messages: messages,
+    //   max_tokens: options.max_tokens,
+    //   temperature: options.temperature,
+    //   frequency_penalty: options.frequency_penalty,
+    //   presence_penalty: options.presence_penalty
+    // });
 
-    // const sse = new SSE(event.node.res)
-    // sse.send({
+    // return {
     //   message: prediction.data.choices[0].message,
     //   usage: prediction.data.usage
-    // })
-    // sse.send({ data: prediction.data.choices[0].message })
-
-    return {
-      message: prediction.data.choices[0].message,
-      usage: prediction.data.usage
-    }
+    // }
 
   } catch (err) {
     // @ts-ignore
